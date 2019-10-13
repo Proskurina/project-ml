fbb0ee765dd23e073455e640ea971b1841a59d0b
yana <yana@example.com>
2019-10-13 13:03:21 +0100

    Complete upload_docker script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=yproskurina/prediction-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag prediction-app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
