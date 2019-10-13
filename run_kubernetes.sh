13fe405d6453bb54ff4b0b68d8a441acfa04e67e
yana <yana@example.com>
2019-10-13 15:20:47 +0100

    Added kubernetes_out.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=yproskurina/prediction-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction-app --image=$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/prediction-app 8000:80
 
